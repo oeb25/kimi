8ea398887cd26421309cb33953a16007b3686e98
Oliver Bøving <oliverboving@gmail.com>
2021-02-03 09:19:02 +0100

    include new folders to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/*.tsx", "./src/*.css"],
+  purge: ["./src/**/*.tsx", "./src/**/*.css"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
