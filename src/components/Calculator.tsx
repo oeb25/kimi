0445fa55676246ea5cd0d6c5dcb1f8f2dbc90794
Oliver Bøving <oliverboving@gmail.com>
2021-02-20 13:23:30 +0100

    update calculator

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,40 +1,113 @@
 import * as React from "react";
 import * as mathjs from "mathjs";
+import { Katex } from "./Katex";
 
-mathjs.createUnit("amu", "0.000000000000000000000001660539g");
-mathjs.createUnit("M", "1 mol/l");
-const Na = mathjs.evaluate("6.02214076e23 mol^-1");
-const R = mathjs.evaluate("0.0821 L atm/(K mol)");
+const math = mathjs.create(mathjs.all) as typeof mathjs;
+
+const idealGas = (
+  _args: mathjs.MathNode[],
+  math: typeof mathjs,
+  scope: Record<string, any>
+) => {
+  if (!("V" in scope)) {
+    return math.evaluate("n R T / p", scope);
+  } else if (!("p" in scope)) {
+    return math.evaluate("n R T / V", scope);
+  } else if (!("n" in scope)) {
+    return math.evaluate("p V / (R T)", scope);
+  } else if (!("T" in scope)) {
+    return math.evaluate("p V / (R n)", scope);
+  } else {
+    return NaN;
+  }
+};
+idealGas.rawArgs = true;
+
+try {
+  math.createUnit("amu", "0.000000000000000000000001660539g");
+  math.createUnit("M", "1 mol/l");
+  math.import({ idealGas, ig: idealGas }, {});
+} catch (e) {
+  console.error(e);
+}
+const Na = math.evaluate("6.02214076e23 mol^-1");
+const R = math.evaluate("0.0821 L atm/(K mol)");
 
 export const Calculator: React.FC<{ precision: number }> = ({ precision }) => {
   const [input, setInput] = React.useState("");
 
-  let res = null;
-  try {
-    res = mathjs.evaluate(input, {
+  const [evalulated, scope] = React.useMemo(() => {
+    const scope = {
       Na,
       R,
-    });
-  } catch (e) {}
+    };
+
+    return [
+      input.split("\n").map((line) => {
+        let res = null;
+        try {
+          res = math.evaluate(line, scope);
+        } catch (e) {
+          console.error(e);
+        }
+
+        return [line, res] as const;
+      }),
+      scope,
+    ] as const;
+  }, [input]);
 
   return (
     <div className="grid grid-flow-col gap-2 border-gray-900 shadow place-self-center place-items-center">
-      <div className="relative p-2 pr-0">
-        <input
-          type="text"
-          className="absolute top-0 left-0 right-0 w-full p-2 text-center bg-transparent"
+      <div className="relative font-mono">
+        <div
+          className="z-0 p-2 pointer-events-none opacity-20"
+          style={{ minWidth: "10em" }}
+        >
+          {evalulated.map(([line, res], i) => (
+            <div key={i}>
+              {res ? (
+                <>
+                  <span className="pointer-events-none">{line}</span>
+                  <span> = </span>
+                  <span className="select-all">
+                    {math.format(res, { precision })}
+                  </span>
+                </>
+              ) : (
+                line || "..."
+              )}
+            </div>
+          ))}
+        </div>
+        <textarea
+          className="absolute top-0 left-0 z-10 w-full h-full p-2 text-left bg-transparent resize-none"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Calculator"
         />
-        <span className="text-transparent">{input || "Calculator"}w</span>
       </div>
-      {res != null && (
-        <>
-          <span>=</span>
-          <span>{mathjs.format(res, { precision })}</span>
-        </>
-      )}
+      {/* <div>
+        {Object.entries(scope)
+          .filter(([key]) => !["Na", "R"].includes(key))
+          .map(([key, value]) => {
+            let tex = "nope";
+            try {
+              const parsed = mathjs.parse(mathjs.format(value));
+              console.log(parsed);
+
+              tex = parsed.toTex();
+            } catch (e) {
+              console.error(e);
+            }
+
+            return (
+              <div>
+                <Katex src={`${key} = ${tex}`} />
+              </div>
+            );
+          })}
+      </div> */}
     </div>
   );
 };
